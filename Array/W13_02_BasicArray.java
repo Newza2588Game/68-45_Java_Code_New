d25f5f5baaa0fb934bade14155c23c67259d762b
Newza2588 Game <newza2588.p@gmail.com>
2025-09-10 11:11:37 +0700

    add new array folder

---
+++ Array/W13_02_BasicArray.java
@@ -0,0 +1,8 @@
+package Array;
+
+public class W13_02_BasicArray {
+    public static void main(String[] args) {
+        String animals[] = {"Cat", "Dog", "Elephant", "Tiger", "Lion"};
+        int dice[] = {1, 2, 3, 4, 5, 6};
+    }
+}
