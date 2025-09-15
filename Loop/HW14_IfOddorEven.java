d8382b52bc8391ce70280bb67cdf3011ee16eec3
Newza2588 Game <newza2588.p@gmail.com>
2025-09-15 15:30:08 +0700

    Add HW14_IfOddorEven.java to implement odd/even number checking functionality

---
+++ Loop/HW14_IfOddorEven.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class HW14_IfOddorEven {
+
+    public static void main(String[] args) {
+        int startingNumber, endingNumber, i;
+        Scanner kb = new Scanner(System.in);
+
+        do {
+            System.out.print("Please enter starting number : ");
+            startingNumber = kb.nextInt();
+            System.out.print("Please enter ending number : ");
+            endingNumber = kb.nextInt();
+            if (startingNumber < endingNumber) {
+                for (i = startingNumber; i <= endingNumber; i++) {
+                    if (i % 2 == 0) {
+                        System.out.println(i + " is an Even number.");
+                    } else {
+                        System.out.println(i + " is an Odd number.");
+                    }
+                }
+            } else {
+                System.out.println("Your starting number must be less than ending number.");
+                System.out.println("===============================");
+            }
+        } while (startingNumber >= endingNumber);
+
+        
+
+    }
+}
