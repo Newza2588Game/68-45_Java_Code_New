8303d421fda6fc83927c536b654d3179eb6eac28
Newza2588 Game <newza2588.p@gmail.com>
2025-10-01 01:28:08 +0700

    Refactor HW15_ArrayCount_OddEven.java to use traditional for loop for array iteration

--- Array/HW15_ArrayCount_OddEven.java
+++ Array/HW15_ArrayCount_OddEven.java
@@ -3,12 +3,12 @@ package Array;
 public class HW15_ArrayCount_OddEven {
 
     public static void main(String[] args) {
-        int[] arr = {7 ,9 ,-1 ,4 ,12 ,9 ,3 ,2 ,-7 ,2 ,1 ,9 ,-15 ,24 ,11 ,13};
+        int arr[] = {7 ,9 ,-1 ,4 ,12 ,9 ,3 ,2 ,-7 ,2 ,1 ,9 ,-15 ,24 ,11 ,13};
         int oddCount = 0;
         int evenCount = 0;
 
-        for (int num : arr) {
-            if (num % 2 == 0) {
+        for (int i=0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
                 evenCount++;
             } else {
                 oddCount++;
